0554ba1bf76d460ec4001beaa4eb49ea64126267
ManishxNinja <manishkaushik2243@gmail.com>
2025-07-10 22:52:43 +0530

    remove credentials

--- apps/excelidraw-frontend/components/AuthPage.tsx
+++ apps/excelidraw-frontend/components/AuthPage.tsx
@@ -3,7 +3,8 @@
 export function AuthPage({isSignin}: {
     isSignin: boolean
 }) {
-    return <div className="w-screen h-screen flex justify-center items-center">
+    
+    return <div className="w-screen h-screen flex justify-center items-center bg-black">
         <div className="p-6 m-2 bg-white rounded">
             <div className="p-2 border rounded mt-2">
                 <input type="text" placeholder="Email"></input>
@@ -12,8 +13,12 @@ export function AuthPage({isSignin}: {
                 <input type="text" placeholder="Password"></input>
             </div>
 
+            <div className={`${!isSignin? "visible" : "hidden"} p-2 border rounded mt-2`} >
+                <input type="text" placeholder="UserName"></input>
+            </div>
+
             <div className="pt-2">
-                <button className="bg-red-200 rounded p-2" onClick={() => {
+                <button className="bg-blue-200 rounded p-2" onClick={() => {
                     
                 }}>{isSignin ? "Sign in" : "Sign up"}</button>
             </div>
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "@repo/backend-common/config";
+import { JWT_SECRET, TOKEN } from "@repo/backend-common/config";
 import dotenv from "dotenv";
 
 dotenv.config()
@@ -9,10 +9,10 @@ interface JwtPayload {
     userId: string
 }
 export function middleware(req: Request, res: Response, next: NextFunction) {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkMTNjNTNhNS1jMDBhLTQyNDUtOTEzOS1jNjA2NzcwNTQ4ODQiLCJpYXQiOjE3NTE3ODUzMTR9.TBWYVdcJxk4kG0yJtDZGw3_ACoT55SfzvxkWWZ6cfeQ";
+
     console.log(process.env.TOKEN)
     console.log(JWT_SECRET);
-    const decoded = jwt.verify(token, "123454") as JwtPayload;
+    const decoded = jwt.verify(TOKEN, "123454") as JwtPayload;
 
     if (decoded) {
         req.userId = decoded.userId;
