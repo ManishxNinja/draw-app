92acd37aa0de3e9bf03dfa7981ae4d733063759f
ManishxNinja <manishkaushik2243@gmail.com>
2025-07-17 17:07:50 +0530

    Added Zod validation in frontend on sigin and signup Page.

--- apps/excelidraw-frontend/components/AuthPage.tsx
+++ apps/excelidraw-frontend/components/AuthPage.tsx
@@ -1,9 +1,53 @@
 "use client";
 
+import { CreateUserSchema, SigninSchema} from "@repo/common/types";
+import { useState } from "react";
+
 export function AuthPage({isSignin}: {
     isSignin: boolean
 }) {
-    
+    const [userData,setUserData] = useState({
+        Email: "",
+        Password: "",
+        UserName: ""
+    });
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setUserData(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleClick = () => {
+        if(isSignin) {
+            const result = SigninSchema.safeParse({
+                username: userData.Email,
+                password: userData.Password,
+            })
+
+            if(!result.success) {
+                alert("Invalid Signin credentials");
+                console.log(result.error.format());
+                return;
+            }
+
+            console.log("Signin data valid", result.data);
+        } else {
+            const result = CreateUserSchema.safeParse({
+                username: userData.Email,
+                password: userData.Password,
+                name: userData.UserName,
+            });
+            if(!result.success) {
+                alert("Invalid Signup credentials");
+                console.log(result.error.format());
+                return;
+            }
+
+            console.log("Signup data valid", result.data);
+        }
+    }
     return <div className="w-screen h-screen flex justify-center items-center bg-black">
         <div className="p-6 m-2 bg-white rounded">
             <div className="p-2 border rounded mt-2">
@@ -18,9 +62,7 @@ export function AuthPage({isSignin}: {
             </div>
 
             <div className="pt-2">
-                <button className="bg-blue-200 rounded p-2" onClick={() => {
-                    
-                }}>{isSignin ? "Sign in" : "Sign up"}</button>
+                <button className="bg-blue-200 rounded p-2" onClick={handleClick}>{isSignin ? "Sign in" : "Sign up"}</button>
             </div>
         </div>
     </div>
